aafd4365c606c7ea6a0d9d1ba9a73c5cc82f2cb4
federicoviscomi <federicoviscomi@gmail.com>
2021-01-22 20:47:44 +0000

    reformat

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -13,8 +13,8 @@ import java.util.UUID;
 @ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
     public final String BEER_PATH_V1 = "/api/v1/beer/";
-    private String apihost;
     private final RestTemplate restTemplate;
+    private String apihost;
 
     public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
